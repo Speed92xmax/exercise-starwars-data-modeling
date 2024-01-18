20f69b9e4ea9f37342d6a100c2ba4deae03fa273
Kevin Pacheco <kevinjp6@gmail.com>
2024-01-18 20:15:55 +0000

    exercise done

--- src/models.py
+++ src/models.py
@@ -7,29 +7,110 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Product (Base):
-    __tablename__="product"
 
-    id= Column(Integer,primary_key=True)
-    name= Column(String(100),nullable=False)
-    details= Column(String(250),nullable=False)
-    image_src= Column(String(250),nullable=False)
-    price=Column(Integer,nullable=True)
-    category_product =relationship("category_product")
+class User (Base) :
+    __tablename__="user"
 
-class Category(Base):
-    __tablename__="category"
+    id = Column ( Integer, primary_key=True)
+    name = Column (String(100),nullable=True)
+    email = Column (String(100),nullable=False)
+    password = Column (String(100),nullable=False)
+    favorite_list = relationship("favorite_list")
 
-    id= Column(Integer,primary_key=True)
-    name= Column (String(100),nullable=False)
-    description = Column(String(250),nullable=True)
-    category_product =relationship("category_product")
+class FavoriteList (Base):
+    __tablename__ = "favorite_list"
 
-class CategoryProduct (Base):
-    __tablename__="category_product"
+    id = Column ( Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey("user.id"),nullable=False)
+    planet_id = Column (Integer, ForeignKey("planet.id"),nullable=False)
+    character_id = Column (Integer, ForeignKey("character.id"),nullable=False)
+    vehicle_id = Column (Integer, ForeignKey("vechicle.id"),nullable=False)
 
-    id= Column( Integer,primary_key=True)
-    product_id= Column(Integer, ForeignKey("product.id"),nullable=False)
-    category_id= Column(Integer, ForeignKey("category.id"),nullable=False)
+
+class Character (Base):
+    __tablename__="character"
+
+    id = Column ( Integer, primary_key=True)
+    msg = Column (String(100),nullable=False)
+    properties_id = Column (Integer, ForeignKey("properties_char.id"),nullable=False)
+    favorite_list = relationship("favorite_list")
+
+class PropertiesChar (Base):
+
+    __tablename__="properties_char"
+
+    id = Column ( Integer, primary_key=True)
+    height = Column(Integer,nullable=True)
+    mass = Column(Integer,nullable=True)
+    name = Column(String(100),nullable=True)
+    hair_color = Column(String(100),nullable=True)
+    home_planet = relationship("planet_list")
+    vechile = relationship("vehicle_list")
+    character= relationship("character")
+
+class Vehicle (Base):
+    __tablename__="vehicle"
+
+    id = Column ( Integer, primary_key=True)
+    msg = Column (String(100),nullable=False)
+    properties_id = Column (Integer, ForeignKey("properties_vehicle.id"),nullable=False)
+    favorite_list = relationship("favorite_list")
+    vehicle_list = relationship("vehicle_list")
+
+class PropertiesVehicle (Base):
+
+    __tablename__="properties_vehicle"
+
+    id = Column ( Integer, primary_key=True)
+    cost_in_credits = Column(Integer,nullable=True)
+    lenght = Column(Integer,nullable=True)
+    model = Column(String(100),nullable=True)
+    vehicle_class = Column(String(100),nullable=True)
+    consumables = Column(String(100),nullable=True )
+    manufacturer = Column(String(100),nullable=True )
+    name = Column(String(100),nullable=True)
+    vechile = relationship("vehicle")
+
+class VehicleList (Base):
+    __tablename__="vehicle_list"
+
+    id = Column ( Integer, primary_key=True)
+    vehicle_id = Column ( Integer, ForeignKey("vehicle.id"))
+    character_id = Column ( Integer, ForeignKey("character.properties_char.id"))
+
+
+class Planet (Base):
+    __tablename__="planet"
+
+    id = Column ( Integer, primary_key=True)
+    msg = Column (String(100),nullable=False)
+    properties_id = Column (Integer, ForeignKey("properties_planet.id"),nullable=False)
+    favorite_list = relationship("favorite_list")
+    planet_list = relationship("planet_list")
+
+class PropertiesPlanet (Base):
+
+    __tablename__="properties_planet"
+
+    id = Column ( Integer, primary_key=True)
+    diameter= Column(Integer,nullable=True)
+    rotation_period= Column(Integer,nullable=True)
+    orbital_period= Column(Integer,nullable=True)
+    gravity = Column(String(100),nullable=True)
+    population= Column(Integer,nullable=True)
+    climate = Column(String(100),nullable=True)
+    name = Column(String(100),nullable=True)
+    planet = relationship("planet")
+
+class PlanetList (Base):
+    __tablename__="planet_list"
+
+    id = Column ( Integer, primary_key=True)
+    planet_id = Column ( Integer, ForeignKey("planet.id"))
+    character_id = Column ( Integer, ForeignKey("character.properties_char.id"))
+
+
+
 
 render_er(Base,'diagam.png')
+
